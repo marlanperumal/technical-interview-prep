bdcdac45da7c0396188bdb5a9f1d707278f1119e
Marlan Perumal <marlan.perumal@gmail.com>
2025-08-13 12:22:35 +0200

    coin-change: implemented dp algorithm

--- coin-change/main.py
+++ coin-change/main.py
@@ -1,5 +1,8 @@
+from functools import cache
+
+
 def make_change_greedy(denominations: list[int], amount: int) -> int:
-    sorted_denominations = reversed(sorted(denominations))
+    sorted_denominations = sorted(denominations, reverse=True)
 
     num_coins = 0
     for i in sorted_denominations:
@@ -15,7 +18,32 @@ def make_change_greedy(denominations: list[int], amount: int) -> int:
     return -1
 
 
+def make_change_dp(denominations: list[int], amount: int) -> int:
+    @cache
+    def make_change(amount: int, value: int = 0) -> int:
+        if amount == value:
+            return 1
+
+        if value > amount:
+            return -1
+
+        change_values = [
+            make_change(amount - value, next_value) for next_value in denominations
+        ]
+        valid_change_values = [i for i in change_values if i > 0]
+        if len(valid_change_values) > 0:
+            return min(valid_change_values) + (1 if value > 0 else 0)
+
+        return -1
+
+    return make_change(amount)
+
+
 if __name__ == "__main__":
     print(make_change_greedy([1, 2, 5], 8))
     print(make_change_greedy([1, 2, 5], 18))
     print(make_change_greedy([1, 2, 5, 7], 10))
+
+    print(make_change_dp([1, 2, 5], 8))
+    print(make_change_dp([1, 2, 5], 18))
+    print(make_change_dp([1, 2, 5, 7], 10))
